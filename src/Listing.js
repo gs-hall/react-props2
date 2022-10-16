b8f8a7aaa9525380bb9c7b299ad71a11aa1d9ec2
Egor Ivantsov <E.Ivantsov@mobilepark.ru>
2022-10-17 01:21:58 +0300

    Added filter by 'state' field

--- src/Listing.js
+++ src/Listing.js
@@ -2,7 +2,7 @@ import React from "react";
 import ListItem from "./ListItem";
 
 export default function Listing({items = []}) {
-  const ItemElements = items.map((item, index) => (
+  const ItemElements = items.filter(item => item.state === "active").map((item, index) => (
     <ListItem item={item} key={item.listing_id} />
   ));
   return (
